7a206063f608e6420a9fa76281b412999239fb56
arch0047 <48592482+arch0047@users.noreply.github.com>
2020-03-25 23:05:22 +0100

    Html file

---
+++ src/test/java/com/example/web_shop1/WebShop1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web_shop1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebShop1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
